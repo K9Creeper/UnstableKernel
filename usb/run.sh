7e0f4a1f3f4a4b47f97202c1d986d927e81186c6
Nick K <97364121+K9Creeper@users.noreply.github.com>
2024-11-20 07:13:22 -0500

    Update run.sh

--- usb/run.sh
+++ usb/run.sh
@@ -4,7 +4,7 @@ echo "--------------------------"
 echo "Creating Your Image"
 echo "--------------------------"
 
-grub-mkrescue -o os.iso
+grub-mkrescue -o os.iso boot
 
 echo "--------------------------"
 echo "Completed Your Image"
