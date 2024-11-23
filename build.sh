1714ed4c457634acae995bf66b1c0777d7f90975
Nick K <nickklapal@gmail.com>
2024-11-23 15:51:55 -0500

    Fixed build.sh;

--- build.sh
+++ build.sh
@@ -6,89 +6,76 @@ export PATH="$PREFIX/bin:$PATH"
 
 export PATH="$HOME/opt/cross/bin:$PATH"
 
-asmCompile(){ for file in "$@"; do
+asmCompile(){
+    for file in "$@"; do
+        echo "ASSEMBLY: Compiling $file to build/bin/AS_$(basename "$file" .s).o"
         $TARGET-as "$file" -o "build/bin/AS_$(basename "$file" .s).o"
-        echo "ASSEMBLY...   Compiled $file to build/bin/AS_$(basename "$file" .s).o"
     done
 }
 
 cCompile(){
     for file in "$@"; do
+        echo "GCC: Compiling $file to build/bin/GCC_$(basename "$file" .c).o"
         $TARGET-gcc -c "$file" -o "build/bin/GCC_$(basename "$file" .c).o" -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-        echo "GCC...    Compiled $file to build/bin/GCC_$(basename "$file" .c).o"
     done
 }
 
 cleanBuild(){
+    echo "Cleaning up previous build files..."
     rm -f build/bin/*.o
     rm -f build/*.bin
 }
 
 clear
-
 ./clean.sh
 
-echo "------------------"
-echo "Compilation Begin"
+echo "-------------------------------"
+echo "Compilation Process Started"
+echo "-------------------------------"
 echo ""
 
-echo ""
+# Assembly Compilation
 echo "ASSEMBLY Compilation"
-echo "------------------"
-echo ""
+echo "---------------------"
 asmCompile $(find source/ -name "*.s")
+echo ""
 
-echo ""
-echo ""
+# GCC Compilation
 echo "GCC Compilation"
-echo "------------------"
-echo ""
-
+echo "---------------"
 cCompile $(find source/ -name "*.c")
-
-echo ""
-echo ""
-echo "Linker Compilation"
-echo "------------------"
 echo ""
 
+# Linker Compilation
+echo "Linking with GCC"
+echo "----------------"
 i686-elf-gcc -T linker.ld -o build/build.bin -ffreestanding -O2 -nostdlib build/bin/GCC_kernel.o build/bin/AS_boot.o -lgcc
+echo ""
 
+echo "-------------------------------"
+echo "Compilation Ended"
+echo "-------------------------------"
 echo ""
-echo ""
-echo "Compilation End"
-echo "------------------"
 
-echo ""
-echo ""
 echo "Copying build/build.bin to usb/boot/build.bin"
-
 cp build/build.bin iso/boot/build.bin
-
 cleanBuild
-
-echo ""
-echo ""
-echo "------------------"
-echo "GRUB-MKIMAGE"
-echo ""
 echo ""
 
+# GRUB-MKIMAGE
+echo "-------------------------------"
+echo "GRUB-MKIMAGE Process"
+echo "-------------------------------"
 $HOME/src/grub-for-windows/grub-mkimage.exe -o iso/boot/grub/grub.img -O i386-pc -p iso/boot/grub iso9660 part_msdos normal linux
-
-echo ""
-echo ""
-echo "------------------"
-
-echo ""
-echo ""
-echo "------------------"
-echo "Genisoimage"
-echo ""
 echo ""
 
+# Genisoimage
+echo "-------------------------------"
+echo "Genisoimage Process"
+echo "-------------------------------"
 genisoimage -o iso/os.iso -b boot/grub/grub.img -no-emul-boot -boot-load-size 4 -boot-info-table iso
+echo ""
 
-echo ""
-echo ""
-echo "------------------"
+echo "-------------------------------"
+echo "Build Complete!"
+echo "-------------------------------"
