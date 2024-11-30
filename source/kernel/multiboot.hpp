a43e64c62c9f311814615ca30efe6ea63f5ac023
Nick K <97364121+K9Creeper@users.noreply.github.com>
2024-11-30 17:23:47 -0500

    Update multiboot.hpp

--- source/kernel/multiboot.hpp
+++ source/kernel/multiboot.hpp
@@ -8,45 +8,7 @@ namespace Kernel
 {
     namespace Multiboot
     {
-        struct ModuleInfo
-        {
-            unsigned mod_start;
-            unsigned mod_end;
-            char* cmdline;
-        };
-
-        struct MmapEntry
-        {
-            uint64_t base_addr;
-            uint64_t length;
-            uint32_t type;
-        };
-
-        struct FramebufferInfo
-        {
-            uint32_t addr;
-            uint32_t width;
-            uint32_t height;
-            uint32_t bpp;
-        };
-
-        class ParsedMultibootInfo
-        {
-            public:
-
-            ~ParsedMultibootInfo();
-
-            char* cmdline;
-            char* bootloader_name;
-            // ModuleInfo* 
-            ordered_array_t modules;
-            uint32_t mem_lower = 0;
-            uint32_t mem_upper = 0;
-            uint32_t boot_device = 0;
-            // MmapEntry* 
-            ordered_array_t mmap_entries;
-            FramebufferInfo framebuffer;
-        };
+        // Take a look here: https://github.com/Phil0nator/OS/blob/main/src/impl/kernel/multiboot_loader.c
 
         extern ParsedMultibootInfo ParseTags(uint32_t addr);
     }
