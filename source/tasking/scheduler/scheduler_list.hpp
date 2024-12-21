ecba7255bb2b2bfcc1046590b2d115a230127cb5
Nick K <97364121+K9Creeper@users.noreply.github.com>
2024-12-21 11:05:27 -0500

    Create scheduler_list.hpp

---
+++ source/tasking/scheduler/scheduler_list.hpp
@@ -0,0 +1,12 @@
+/// ------------------
+/// scheduler_list.hpp
+/// @breif This file declares the class SchedulerList.
+
+#pragma once
+
+#include "../../cpphelperes/ordered_array"
+
+class SchedulerList : public OrderedArray{
+
+
+}
