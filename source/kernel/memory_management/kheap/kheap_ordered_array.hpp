50c926bf9e5e6dba997ff5ebe3caab16148f1713
Nick K <97364121+K9Creeper@users.noreply.github.com>
2024-12-21 11:11:03 -0500

    Update kheap_ordered_array.hpp

--- source/kernel/memory_management/kheap/kheap_ordered_array.hpp
+++ source/kernel/memory_management/kheap/kheap_ordered_array.hpp
@@ -2,7 +2,8 @@
 
 #include "../../../cpphelpers/ordered_array.hpp"
 
-
 class KHeapOrderedArray : public OrderedArray<void *> {
   bool lessthan_(void *a, void *b);
 };
+
+template class OrderedArray<void*>;
